df3a400f321c5fec8c3611dd860dc29407d6ef1c
Victoria_Ivanova <iv.victoriya@gmail.com>
2020-11-28 17:45:16 +0400

    Запуск и закрытие браузера

--- src/test/java/calcTests/Tests.java
+++ src/test/java/calcTests/Tests.java
@@ -1,4 +1,16 @@
 package calcTests;
 
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Tests {
+
+    @Test
+    public void Test1() {
+        System.setProperty("webdriver.chrome.driver", "C:/webdrivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+        driver.quit();
+    }
 }
