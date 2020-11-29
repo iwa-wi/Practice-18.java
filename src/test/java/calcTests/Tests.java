fedcb4b8940890cf30910656e6b502c3b11cba80
Victoria_Ivanova <iv.victoriya@gmail.com>
2020-11-29 23:48:03 +0400

    Три теста

--- src/test/java/calcTests/Tests.java
+++ src/test/java/calcTests/Tests.java
@@ -35,14 +35,43 @@ public class Tests {
         driver.get("http://google.com");
         googleSearch.search("Calculator");
         driver.findElement(By.cssSelector("div.jlkklc")).sendKeys("(1+2)*3-40/5", Keys.ENTER);
-        WebDriverWait wait = new WebDriverWait(driver, 100);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.vUGUtc")));
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#cwos.qv3Wpe")));
         Assertions.assertAll(
                 () -> assertEquals("(1 + 2) * 3 - 40 ÷ 5 =", CalcPage.getHistory()),
                 () -> assertEquals("1", CalcPage.getResults())
         );
     }
 
+    @Test
+    public void Test2() {
+        driver.get("http://google.com");
+        googleSearch.search("Calculator");
+        driver.findElement(By.cssSelector("div.jlkklc")).sendKeys("6/0", Keys.ENTER);
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#cwos.qv3Wpe")));
+        Assertions.assertAll(
+                () -> assertEquals("6 ÷ 0 =", CalcPage.getHistory()),
+                () -> assertEquals("Infinity", CalcPage.getResults())
+        );
+    }
 
+    @Test
+    public void Test3() {
+        driver.get("http://google.com");
+        googleSearch.search("Calculator");
+        driver.findElement(By.cssSelector("div.jlkklc")).sendKeys("sin", Keys.ENTER);
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#cwos.qv3Wpe")));
+        Assertions.assertAll(
+                () -> assertEquals("sin() =", CalcPage.getHistory()),
+                () -> assertEquals("Error", CalcPage.getResults())
+        );
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }
 
 }
